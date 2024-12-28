e651ee759f875c61b15d9e5a3a4ea2876f661962
elektro <elektronikotin@gmail.com>
2024-12-28 23:25:57 +0300

    Remove 'Mod' keyword

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // Tema tercihini yerel depolamada kontrol et
     if (localStorage.getItem('theme') === 'dark') {
         body.classList.add('dark-mode');
-        toggleLink.textContent = 'Açık Mod';
+        toggleLink.textContent = 'Açık';
     }
 
     toggleLink.addEventListener('click', (e) => {
         e.preventDefault();
         body.classList.toggle('dark-mode');
         if (body.classList.contains('dark-mode')) {
-            toggleLink.textContent = 'Açık Mod';
+            toggleLink.textContent = 'Açık';
             localStorage.setItem('theme', 'dark');
         } else {
-            toggleLink.textContent = 'Koyu Mod';
+            toggleLink.textContent = 'Koyu';
             localStorage.setItem('theme', 'light');
         }
     });
